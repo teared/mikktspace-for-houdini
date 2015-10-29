25d1fce0cb52bc2af68871ae7d523c325f8abfdd
teared <teared@users.noreply.github.com>
2015-10-29 15:18:56 +0500

    Updated for Houdini 15
    Added bitangent calculation in Basic mode to save couple of SOPs.

--- source/SOP_ComputeTangents.c
+++ source/SOP_ComputeTangents.c
@@ -15,7 +15,6 @@ PRM_Template  SOP_ComputeTangents::myTemplateList[] = {
     PRM_Template()
 };
 
-
 OP_Node *SOP_ComputeTangents::myConstructor(OP_Network *net, const char *name, OP_Operator *op)
 {
     return new SOP_ComputeTangents(net, name, op);
@@ -41,15 +40,15 @@ OP_ERROR SOP_ComputeTangents::cookMySop(OP_Context &context)
 
     // Perform basic checks to avoid segfaults. We won't fix anything here
     // because it is easier to do inside Houdini.
-    GA_ROHandleV3 n(gdp, GA_ATTRIB_VERTEX, "N");
-    if (n.isInvalid())
+    GA_ROHandleV3 normalHandle(gdp, GA_ATTRIB_VERTEX, "N");
+    if (normalHandle.isInvalid())
     {
         addError(SOP_ERR_INVALID_SRC, "(no vertex normals)");
         return error();
     }
 
-    GA_ROHandleV3 uv(gdp, GA_ATTRIB_VERTEX, "uv");
-    if (uv.isInvalid())
+    GA_ROHandleV3 uvHandle(gdp, GA_ATTRIB_VERTEX, "uv");
+    if (uvHandle.isInvalid())
     {
         addError(SOP_ERR_INVALID_SRC, "(no vertex uvs)");
         return error();
@@ -70,15 +69,29 @@ OP_ERROR SOP_ComputeTangents::cookMySop(OP_Context &context)
     if (basic)
     {
         GA_RWHandleV3 tangentuHandle(gdp->addFloatTuple(GA_ATTRIB_VERTEX, "tangentu", 3));
-
-        // Change type to "vector" from "3 floats". It allows to retain good
-        // tangent directions after transforming geometry with changing the
-        // normals itself. PolyFrame SOP also uses such vectors for tangents.
-        gdp->findAttribute(GA_ATTRIB_VERTEX, "tangentu")->setTypeInfo(GA_TYPE_VECTOR);
-
+        GA_RWHandleV3 tangentvHandle(gdp->addFloatTuple(GA_ATTRIB_VERTEX, "tangentv", 3));
         GA_RWHandleF signHandle(gdp->addFloatTuple(GA_ATTRIB_VERTEX, "sign", 1));
+
+        // Change type to "normal" from "3 floats". It allows to retain proper
+        // tangents directions after transforming geometry. PolyFrame SOP uses
+        // vectors for tangents, which is inconsistent with normals.
+        gdp->findAttribute(GA_ATTRIB_VERTEX, "tangentu")->setTypeInfo(GA_TYPE_NORMAL);
+        gdp->findAttribute(GA_ATTRIB_VERTEX, "tangentv")->setTypeInfo(GA_TYPE_NORMAL);
+
         Calculator().callMorty(gdp, basic);
+
+        // Calculate "basic" tangentv.
+        for (GA_Iterator i(gdp->getVertexRange()); !i.atEnd(); i.advance())
+        {
+            UT_Vector3F normal, tangentu, tangentv;
+            normal = normalHandle.get(*i);
+            tangentu = tangentuHandle.get(*i);
+            tangentv = signHandle.get(*i) * cross(normal, tangentu);
+            tangentvHandle.set(*i, tangentv);
+        }
+
         tangentuHandle.bumpDataId();
+        tangentvHandle.bumpDataId();
         signHandle.bumpDataId();
     }
     else
@@ -86,8 +99,8 @@ OP_ERROR SOP_ComputeTangents::cookMySop(OP_Context &context)
         // Looks like a gun.
         GA_RWHandleV3 tangentuHandle(gdp->addFloatTuple(GA_ATTRIB_VERTEX, "tangentu", 3));
         GA_RWHandleV3 tangentvHandle(gdp->addFloatTuple(GA_ATTRIB_VERTEX, "tangentv", 3));
-        gdp->findAttribute(GA_ATTRIB_VERTEX, "tangentu")->setTypeInfo(GA_TYPE_VECTOR);
-        gdp->findAttribute(GA_ATTRIB_VERTEX, "tangentv")->setTypeInfo(GA_TYPE_VECTOR);
+        gdp->findAttribute(GA_ATTRIB_VERTEX, "tangentu")->setTypeInfo(GA_TYPE_NORMAL);
+        gdp->findAttribute(GA_ATTRIB_VERTEX, "tangentv")->setTypeInfo(GA_TYPE_NORMAL);
         GA_RWHandleF maguHandle(gdp->addFloatTuple(GA_ATTRIB_VERTEX, "magu", 1));
         GA_RWHandleF magvHandle(gdp->addFloatTuple(GA_ATTRIB_VERTEX, "magv", 1));
         GA_RWHandleI keepHandle(gdp->addIntTuple(GA_ATTRIB_VERTEX, "keep", 1));
